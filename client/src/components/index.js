e0245ce68400542433c389123ed89eeed25e7efb
Razvan <razvan@inclub.ro>
2017-05-08 08:09:37 +0200

    feat: cart version one

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,6 +3,8 @@ export { default as Errors } from './Errors';
 export { default as Header } from './Header';
 export { default as LinkItem } from './LinkItem';
 export { default as LinksList } from './LinksList';
+export { default as CartItem } from './CartItem';
+export { default as CartList } from './CartList';
 export { default as NavBar } from './NavBar';
 export { default as ShortUrl } from './ShortUrl';
 export { default as ContactForm } from './ContactForm';
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import links from './links';
 import error from './error';
+import cartItems from './cartItems';
 
 const rootReducer = combineReducers({
   links,
+  cartItems,
   error,
 });
 
